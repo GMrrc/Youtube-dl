b2363c266f65f8fe739becd2634b5939368d20aa
GMrrc <143954951+GMrrc@users.noreply.github.com>
2024-04-12 19:35:13 +0200

    Update playlist_dl.py

--- playlist_dl.py
+++ playlist_dl.py
@@ -47,7 +47,7 @@ def Download(link, save_path, index):
     print("Download is completed successfully")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         print("Usage: python script.py <url> <lang>")
         sys.exit(1)
     
